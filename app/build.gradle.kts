5428fcf0f001c214ba9de670f8ee38b74b1bad9b
estherjsong <estherjsong53@gmail.com>
2024-01-06 20:32:38 +0900

    [fix] register tab design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("de.hdodenhof:circleimageview:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
