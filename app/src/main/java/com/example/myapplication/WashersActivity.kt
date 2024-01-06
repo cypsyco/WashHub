cff93d9c4f968faed278c3c19b3ba1f1d6ec7014
estherjsong <estherjsong53@gmail.com>
2024-01-06 13:41:06 +0900

    [feat] WasherList from Server

--- app/src/main/java/com/example/myapplication/WashersActivity.kt
+++ app/src/main/java/com/example/myapplication/WashersActivity.kt
@@ -11,51 +11,34 @@ import retrofit2.Response
 
 class WashersActivity : AppCompatActivity() {
 
-    private val washerList = listOf(
-        Washer(1, "Dryer 1", "USED"),
-        Washer(2, "Dryer 2", "AVAILABLE"),
-        // Add other dryers here...
-    )
-
-
-
-//    val call = RetrofitClient.instance.getWashers()
-//    call.enqueue(object: Callback<List<Washer>> {
-//        override fun onResponse(call: Call<List<Washer>>, response: Response<List<Washer>>) {
-//            if (response.isSuccessful) {
-//                val fetchedList = response.body() // 받아온 데이터는 userList에 저장됩니다.
-//                fetchedList?.forEach { washer ->
-//                    Log.d("WasherList", washer.toString())
-//                    // 여기서 "UserList"는 태그로, "User Name: ${user.name}"은 출력할 내용입니다.
-//                    // 필요에 따라 로그 형식을 변경할 수 있습니다.
-//                }
-//                // userList를 사용하여 작업을 수행합니다.
-//            } else {
-//                // 실패 시 처리
-//            }
-//        }
-//
-//        override fun onFailure(call: Call<List<Washer>>, t: Throwable) {
-//            Log.e("WasherList", "Failed: ${t.message}")
-//        }
-//    })
+    private var washerList = mutableListOf<Washer>()
+//    = listOf(
+//        Washer(1, "Dryer 1", "USED"),
+//        Washer(2, "Dryer 2", "AVAILABLE"),
+//        // Add other dryers here...
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_washers)
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.washers)
+
+        val layoutManager = LinearLayoutManager(this)
 
         val call = RetrofitClient.instance.getWashers()
         call.enqueue(object: Callback<List<Washer>> {
         override fun onResponse(call: Call<List<Washer>>, response: Response<List<Washer>>) {
             if (response.isSuccessful) {
                 Log.d("WasherList", response.body().toString())
-//                val fetchedList = response.body() // 받아온 데이터는 userList에 저장됩니다.
-//                fetchedList?.forEach { washer ->
-//                    Log.d("WasherList", washer.toString())
-//                    // 여기서 "UserList"는 태그로, "User Name: ${user.name}"은 출력할 내용입니다.
-//                    // 필요에 따라 로그 형식을 변경할 수 있습니다.
-//                }
-                // userList를 사용하여 작업을 수행합니다.
+                val fetchedList = response.body()
+                fetchedList?.let{
+                    washerList.clear()
+                    washerList.addAll(it)
+                    recyclerView.adapter?.notifyDataSetChanged()
+                }
+                Log.d("WasherList2", washerList.toString())
             } else {
                 // 실패 시 처리
             }
@@ -66,9 +49,6 @@ class WashersActivity : AppCompatActivity() {
         }
     })
 
-        val recyclerView = findViewById<RecyclerView>(R.id.washers)
-
-        val layoutManager = LinearLayoutManager(this)
 
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = WasherAdapter(washerList)
